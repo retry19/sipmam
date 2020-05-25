fdcd8b08f0e1a1255302e42e58854f6c553202bc
Reza Rachmanuddin <rezarahmanudin@gmail.com>
2020-05-25 11:48:02 +0700

    menambah tabel menu
    membuat migration untuk pembuatan tabel menus
    membuat model Menu

---
+++ database/migrations/2020_05_25_043754_create_menus_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_menu', 32);
+            $table->string('foto_menu');
+            $table->enum('jenis_menu', ['makanan', 'minuman']);
+            $table->unsignedInteger('jml_tersedia');
+            $table->unsignedInteger('jml_dipesan');
+            $table->integer('harga');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
