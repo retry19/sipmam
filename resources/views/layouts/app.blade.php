9ee782ca36e5eed01e99821ade37fbfd8051b45f
Reza Rachmanuddin <rezarahmanudin@gmail.com>
2020-05-28 16:38:31 +0700

    mengubah logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,7 +61,7 @@
       <a href="index3.html" class="brand-link elevation-4">
         <img src="{{ asset('img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
             style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <span class="brand-text font-weight-light">SIPMAM</span>
       </a>
 
       <!-- Sidebar -->
